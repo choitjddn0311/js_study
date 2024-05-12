a9de96a9df91732316a7099006b6cb559db8e1af
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2024-05-12 23:59:57 +0900

    feat:js data_types

---
+++ 1_basics/5_data_types.js
@@ -0,0 +1,42 @@
+/**
+ * Data Types
+ * 
+ * 여섯개의 Primitive Type과
+ * 한개의 오브젝트 타입이있다
+ * 
+ * 1) Number (숫자)
+ * 2) String (문자열)
+ * 3) Boolean (불리언)
+ * 4) undefined (언디파인드)
+ * 5) null (널)
+ * 6) symbol (심볼)
+ * 
+ * 7) Object (객체)
+ *    Function
+ *    Array
+ *    Object
+ */
+
+/** 넘버타입 
+ * 
+*/
+const age = 32;
+const tempature = -10;
+const pi = 3.14;
+
+console.log(typeof age);
+console.log(typeof tempature);
+console.log(typeof pi);
+console.log('---------------------');
+
+const infinity = Infinity;
+const nInfinity = -infinity;
+console.log(typeof infinity);
+console.log(typeof nInfinity);
+console.log('---------------------');
+
+/**
+ * String 타입
+ */
+const codeFactory = '코드팩토리';
+console.log(typeof codeFactory);
