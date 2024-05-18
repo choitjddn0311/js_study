45800fa1f70444bda94969cb99186ca4616de3ec
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2024-05-18 23:13:24 +0900

    feat: self loop test

---
+++ self_test/test.js
@@ -0,0 +1,44 @@
+// for문을 사용하여 0부터 10미만의 정수 중에서 짝수만을 작은 수부터 출력하시오.
+
+// for(let number = 0; number < 10; number++) {
+//     if(number % 2 === 0) {
+//         console.log(number);
+//     }
+// }
+
+// for문을 사용하여 0부터 10미만의 정수 중에서 홀수만을 큰수부터 출력하시오.
+
+// for(let i = 10; i > 0; i --) {
+//     if(i % 2 !== 0) {
+//         console.log(i);
+//     }
+// }
+
+// while문을 사용하여 0 부터 10 미만의 정수 중에서 짝수만을 작은 수부터 출력하시오.
+
+// let i = 0;
+
+// while(i < 10){
+//     if(i % 2 === 0) {
+//         console.log(i);
+//     }
+//     i ++;
+// }
+
+// for 문을 사용하여 0부터 10미만의 정수의 합을 출력하시오.
+// let num = 0;
+
+// for(let i = 0; i < 10; i++) {
+//     num = num + i;
+// }
+// cnsole.log(num);
+
+// 1부터 20 미만의 정수 중에서 2 또는 3의 배수인 수의 총합을 구하시오.
+// let i = 0;
+
+// for(let num = 1; num < 20; num++){
+//     if(num % 2 === 0 || num % 3 === 0 ) {
+//         i += num;
+//     }
+// }
+// console.log(i);
