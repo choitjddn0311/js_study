6b1dd39d4ca62ec4fa60d9660b727d04b8314455
choitjddn0311 <cswcjw311427@naver.com>
2024-05-20 20:09:06 +0900

    feat: array

---
+++ 1_basics/13_must_know_array_functions.js
@@ -0,0 +1,23 @@
+/**
+ * Array Functions
+ */
+let suckFamily = [
+    '유현재',
+    '김동균',
+    '김태연',
+    '배준호',
+    '배성호',
+    '최성우',
+];
+
+console.log(suckFamily);
+
+//push() array 맨마지막에 꼬라박기
+console.log(suckFamily.push('코드팩토리'));
+console.log(suckFamily);
+
+console.log('-----------------------------')
+
+//pop()
+console.log(suckFamily.pop());
+console.log(suckFamily);
