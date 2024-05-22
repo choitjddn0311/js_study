426991b95b48eaf5c4755a4d2445eaa9d483542d
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2024-05-23 00:48:58 +0900

    feat : add concat function

--- 1_basics/13_must_know_array_functions.js
+++ 1_basics/13_must_know_array_functions.js
@@ -21,3 +21,32 @@ console.log('-----------------------------')
 //pop()
 console.log(suckFamily.pop());
 console.log(suckFamily);
+
+console.log('--------------------------');
+//shift()
+console.log(suckFamily.shift());
+console.log(suckFamily);
+
+//unshift()
+console.log(suckFamily.unshift('노무현'));
+console.log(suckFamily);
+
+console.log('------------------------');
+
+console.log(suckFamily.splice(0 , 3));
+console.log(suckFamily);
+
+suckFamily = [
+    '유현재',
+    '김동균',
+    '김태연',
+    '배준호',
+    '배성호',
+    '최성우',
+];
+
+console.log(suckFamily);
+
+//concat()
+console.log(suckFamily.concat('코드팩토리'));  //push 랑 기능은 비슷함 근데 array에 저장이 안되고 concat한 부분만 출력됨 다른점임 push랑
+console.log(suckFamily);
