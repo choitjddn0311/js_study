0d1b04b1c64394553d3ef586af15d4079e1541f8
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2024-07-02 22:07:12 +0900

    feat : infinite animation

---
+++ infinite_animation/script.js
@@ -0,0 +1,22 @@
+let circle1 = document.querySelector('.circle1');
+let circle2 = document.querySelector('.circle2');
+
+for(let i = 0; i<=80; i++) {
+    let span1 = document.createElement('span');
+    span1.style.setProperty('--i',i);
+    circle1.appendChild(span1);
+
+    let span2 = document.createElement('span');
+    span2.style.setProperty('--i',i);
+    circle2.appendChild(span2);
+}
+
+
+
+
+
+
+
+
+
+// setProperty 함수는 style의 값 설정하는 내장함수
