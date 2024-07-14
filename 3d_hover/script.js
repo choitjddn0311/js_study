09b38a2aa26071cf0fc597462a1ce300ee448a89
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2024-07-14 23:56:46 +0900

    feat : add 3d hover effect

---
+++ 3d_hover/script.js
@@ -0,0 +1,11 @@
+let list = document.querySelectorAll(".sci li");
+let bg = document.querySelector("body");
+list.forEach(element => {
+    element.addEventListener("mouseenter" , function(event){
+        let color = event.target.style.getPropertyValue('--clr');
+        bg.style.backgroundColor = color;
+    })
+    element.addEventListener("mouseleave" , function(event){
+        bg.style.backgroundColor = '#fff';
+    })
+})
