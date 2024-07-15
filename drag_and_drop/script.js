c3625a315f595db503714a4063fe70bf90442a87
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2024-07-15 23:06:00 +0900

    feat : drag and drop with javaScript

--- drag_and_drop/script.js
+++ drag_and_drop/script.js
@@ -1,26 +1,17 @@
-function console(e) {
-    if(document.querySelectorAll('p').length === 10) {
-        document.querySelectorAll('p').forEach(e => e.remove())
-    }
-    const p = document.createElement('p');
-    p.textContent = e;
-    document.body.append(p);
+let lists =  document.getElementsByClassName("list");
+let rightBox = document.getElementById("right");
+let leftBox = document.getElementById("left");
+
+for(list of lists) {
+    list.addEventListener("dragstart" , function(e) {
+        let selected = e.target;
+
+        rightBox.addEventListener("dragover" , function(e){
+            e.preventDefault();
+        });
+        rightBox.addEventListener("drop" , function(e){
+            rightBox.appendChild(selected);
+            selected = null;
+        })
+    })
 }
-const contain2 = document.querySelector(".contain2")
-const item = document.querySelector(".item");
-
-
-//드래그를 시작할때 발생하는 이벤트 dragstart
-// item.addEventListener("dragstart" , (e) => {
-//     console("드래그를 시작하면 발생하는 이벤트")
-// })
-
-//드래그 중일때 발생하는 이벤트 drag
-// item.addEventListener("drag" , (e) => {
-//     console("드래그를 하면 발생하는 이벤트")
-// })
-
-//드래그를 할때 이 영역에 닿으면 발생하는 이벤트 .dragenter
-// contain2.addEventListener("dragenter" , (e)=> {
-//     console("드래그 요소가 영역에 닿으면 발생하는 이벤트")
-// })
